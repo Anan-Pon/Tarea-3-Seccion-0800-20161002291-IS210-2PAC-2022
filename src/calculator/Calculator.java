90a577ccf1e97a30e28309ed5fa8ccdf4e0e3ae9
Anan-Pon <ananpon@outlook.es>
2022-08-16 23:07:20 -0600

    Se agrega el Proyecto

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author Anan Pon
+ */
+public class Calculator extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Calculadora (Anan Pon - 20161002291)");
+        Image icon = new Image(getClass().getResourceAsStream("math-symbols2.png"));
+        stage.getIcons().add(icon);
+        stage.show();
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
+}
